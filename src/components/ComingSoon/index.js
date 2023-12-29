59ba5248fc9cf2c2b677ccab568c666e38cb7f8e
Noah Becker <102703954+noah35becker@users.noreply.github.com>
2023-12-28 21:43:59 -0700

    responsive design for ComingSoon component

--- src/components/ComingSoon/index.js
+++ src/components/ComingSoon/index.js
@@ -6,9 +6,9 @@ import './index.css';
 // COMPONENT
 function ComingSoon(){
     return (<div className='coming-soon text-center'>
-        <p className='under-reconstruction text-uppercase text-white fw-bold mb-0'>Under Reconstruction</p>
+        <p className='under-reconstruction text-uppercase text-white fw-bold mb-3'>Under Reconstruction</p>
 
-        <p className='coming-soon-msg text-danger fs-2 fw-bold mb-0'>New site coming soon!</p>
+        <p className='coming-soon-msg text-danger fw-bold mb-0'>New site coming soon!</p>
     </div>);
 }
 
